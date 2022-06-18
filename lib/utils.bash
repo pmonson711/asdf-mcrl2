85b585d8d92bcea696b2faa89a05ce73d13dd3bf
pmonson711 <4119175+pmonson711@users.noreply.github.com>
2022-06-18 16:19:01 -0500

    Update utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -42,7 +42,7 @@ download_release() {
   filename="$2"
 
   # TODO: Adapt the release URL convention for mcrl2
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/download/${version}/mcrl2-${version}_x86_64.deb"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
