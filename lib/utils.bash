60751b19a163f4677f17b5229e7c4f37b62dc578
Paul Monson <4119175+pmonson711@users.noreply.github.com>
2022-06-19 09:37:19 -0500

    better unzip

--- lib/utils.bash
+++ lib/utils.bash
@@ -71,9 +71,8 @@ extract_release() {
   if command -v dpkg &>/dev/null; then
     dpkg -x "$release_file" "$download_path" || fail "Could not extract $release_file"
   elif command -v sw_vers &>/dev/null; then
-    hdiutil imageinfo "$release_file" | cat
-    7z x "$release_file"
-    ls -al
+    7z x "$release_file" -o "$download_path"
+    ls -al "$download_path/mcrl2-*"
   else
     fail "currently only dpkg based installs are supported"
   fi
